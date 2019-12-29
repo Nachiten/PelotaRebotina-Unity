185af8acf404ad2a7db82fa502825a1260669fd2
Ignacio Baptista [PC] <ibaptista@est.frba.utn.edu.ar>
2019-12-29 15:04:48 -0300

    Terminado sistema de strikes y correctas, cuando se pierde bolas se detienen

--- Assets/Scripts/movimientoPelota.cs
+++ Assets/Scripts/movimientoPelota.cs
@@ -5,17 +5,35 @@ using UnityEngine.UI;
 
 public class movimientoPelota : MonoBehaviour
 {
+    float velocidadX;
+    float velocidadY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        velocidadX = generarNumeroRandom();
+        velocidadY = 3 - generarNumeroRandom();
+    }
+
+    float generarNumeroRandom()
+    {
+        var random = new Random();
+        float numeroRandom = Random.Range(1f, 3f);
+
+        return numeroRandom;
     }
 
     int strikes = 0;
+    int correctas = 0;
+
+    bool perdio = false;
 
     // Update is called once per frame
     void Update()
     {
+
+        if (perdio) return;
+
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -28,37 +46,49 @@ public class movimientoPelota : MonoBehaviour
                 return;
             }
 
-            if (hit.collider.gameObject.name == "Pelota")
+            if (hit.collider.gameObject.tag == "PelotaCorrecta")
             {
                 if (velocidadX > 0) velocidadX += 1f; else velocidadX -= 1f;
 
                 if (velocidadY > 0) velocidadY += 1f; else velocidadY -= 1f;
+
+                sumarUnaCorrecta();
             }
             else {
                 sumarUnStrike();
             }
         }
 
-        
+
+    }
+
+    void sumarUnaCorrecta() {
+        correctas++;
+
+        GameObject.Find("Cant Correctas").GetComponent<Text>().text = correctas.ToString();
+        //Debug.Log("Cantidad de Correctas: " + strikes);
     }
 
     void sumarUnStrike() {
 
-        if (strikes == 4) return;
-
         strikes++;
-        GameObject.Find("Cant Strikes").GetComponent<Text>().text = strikes.ToString();
-        Debug.Log("Cantidad de STIKES: " + strikes);
-    }
 
-    float velocidadX = 3;
-    float velocidadY = 3;
+        if (strikes == 3)
+        {
+            perdio = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+        GameObject.Find("Cant Strikes").GetComponent<Text>().text = strikes.ToString();
+        //Debug.Log("Cantidad de STIKES: " + strikes);
+    }
 
     void FixedUpdate()
     {
+        if (perdio) return;
+
         this.GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadX, velocidadY);
 
-        //Debug.Log("Velocidad X: " + velocidadX + " | Velocidad Y: " + velocidadY);
+        Debug.Log("Velocidad X: " + velocidadX + " | Velocidad Y: " + velocidadY);
     }
 
     private void OnCollisionEnter2D(Collision2D elementoGolpeado)
